3291b43ff385c2b824ec37d8210651983ed9ac61
Abhinav Katare <therockstarstar695@gmail.com>
2025-09-05 14:00:51 +0530

    Update code_analysis.py

--- ai_service/models/code_analysis.py
+++ ai_service/models/code_analysis.py
@@ -1,7 +1,7 @@
 # For code analysis this code will make an issue basemodel in the model
 from pydantic import BaseModel
 from typing import Optional
-
+ 
 class Issue(BaseModel):
     line: int
     issue: str
